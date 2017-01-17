f104848c82bf73efb071f91f3c578dcf4676b5e9
Jorge Oleques <joleques@umov.me>
2017-01-17 10:13:56 -0200

    Criando o Conversor

---
+++ src/conversor/core/Conversor.java
@@ -0,0 +1,11 @@
+package conversor.core;
+
+import java.math.BigDecimal;
+
+public class Conversor {
+
+	
+	public BigDecimal currencyQuotation(String from, String to, Number value, String quotation){
+		return null;
+	}
+}
---
+++ test/conversor/core/ConversorTest.java
@@ -0,0 +1,11 @@
+package conversor.core;
+
+import org.junit.Before;
+
+public class ConversorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+}
